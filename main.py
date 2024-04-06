4901d2c894ef1f08b850ef1c1ebb841693afd862
loomhub <66206195+loomhub@users.noreply.github.com>
2024-04-06 14:44:10 +0000

    Phrase endpoint

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
-from mylib.logic import wiki
+from fastapi import FastAPI
+import uvicorn
+from mylib.logic import search_wiki
+from mylib.logic import wiki as wikilogic
+from mylib.logic import phrases
 
-print(wiki())
+app = FastAPI()
+@app.get("/")
+async def root():
+    return {"message":"Wikipedia API. Call /wiki or /search"}
+
+@app.get("/search/{name}")
+async def search(name:str):
+    result = search_wiki(name)
+    return {"result":result}
+
+@app.get("/wiki/{name}")
+async def wiki(name:str):
+    result = wikilogic(name)
+    return {"result":result}
+
+@app.get("/phrase/{name}")
+async def phrase(name:str):
+    result = phrases(name)
+    return {"result":result}
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080,host='0.0.0.0')
+
+#print(wiki())
--- mylib/logic.py
+++ mylib/logic.py
@@ -1,6 +1,16 @@
 import wikipedia  # type: ignore
-
+from textblob import TextBlob
 
 def wiki(name="Biology", length=1):
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+def search_wiki(name):
+    result=wikipedia.search(name)
+    return result
+
+def phrases(name):
+    page=wiki(name)
+    blob=TextBlob(page)
+    result=blob.noun_phrases
+    return result
